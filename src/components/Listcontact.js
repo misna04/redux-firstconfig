bf8615a3bf76e654c90873f80cc765e2514bdeda
misna04 <misnasyari@gmail.com>
2022-01-30 13:10:09 +0800

    reducer

---
+++ src/components/Listcontact.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+export default function Listcontact() {
+  return <div>
+      <h2>List Contact</h2></div>;
+}
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import {combineReducers} from 'redux'
+import contactReducer from './contacts'
+
+export default combineReducers({
+    contactReducer
+})
